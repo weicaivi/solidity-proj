3824a26cf9135bf2d8bc2849360cc14488664672
weicaivi <102925394+weicaivi@users.noreply.github.com>
2025-11-02 06:41:53 -0500

    Day 14 submission

---
+++ submissions/vicaiwei/Day14_TimeLockedDepositBox.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0; 
+
+import "./BaseDepositBox.sol";
+
+contract TimeLockedDepositBox is BaseDepositBox {
+    uint256 private unlockTime;
+
+    constructor(uint256 lockDuration) {
+        unlockTime = block.timestamp + lockDuration;
+    }
+
+    modifier timeUnlocked() {
+        require(block.timestamp >= unlockTime, "Box is still locked");
+        _;
+    }
+
+    function getBoxType() public pure override returns (string memory) {
+        return "TimeLocked";
+    }
+
+    function getSecret() public view override onlyOwner timeUnlocked returns (string memory) {
+        return super.getSecret();
+    }
+
+    function getUnlockTime() external view returns (uint256) {
+        return unlockTime;
+    }
+
+    function getRemaininglockTime() external view returns (uint256) {
+        if (block.timestamp >= unlockTime) return 0;
+        return unlockTime - block.timestamp;
+    }
+}
